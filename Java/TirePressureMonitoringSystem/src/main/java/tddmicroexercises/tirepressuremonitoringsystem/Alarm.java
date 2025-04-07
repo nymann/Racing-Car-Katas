186eec349b5dad11fd7236eab8171a4bb2397f79
Kristian Jakobsen <kristian@nymann.dev>
2025-04-07 22:53:58 +0200

    Add shitty tests

--- Java/TirePressureMonitoringSystem/src/main/java/tddmicroexercises/tirepressuremonitoringsystem/Alarm.java
+++ Java/TirePressureMonitoringSystem/src/main/java/tddmicroexercises/tirepressuremonitoringsystem/Alarm.java
@@ -5,7 +5,11 @@ public class Alarm
     private static final double LOW_PRESSURE_THRESHOLD = 17;
     private static final double HIGH_PRESSURE_THRESHOLD = 21;
 
-    protected Sensor sensor = new Sensor();
+    protected Sensor sensor;
+    public Alarm(Sensor sensor) {
+        this.sensor = sensor;
+    }
+
 
     protected boolean alarmOn = false;
 
--- Java/TirePressureMonitoringSystem/src/test/java/tddmicroexercises/tirepressuremonitoringsystem/AlarmTest.java
+++ Java/TirePressureMonitoringSystem/src/test/java/tddmicroexercises/tirepressuremonitoringsystem/AlarmTest.java
@@ -1,14 +1,44 @@
 package tddmicroexercises.tirepressuremonitoringsystem;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 import org.junit.jupiter.api.Test;
 
 public class AlarmTest {
 
     @Test
-    public void foo() {
-        Alarm alarm = new Alarm();
+    public void alarmIsOnWhenSensorReturnsValueBelowThreshold() {
+        Sensor sensor = mock(Sensor.class);
+        when(sensor.popNextPressurePsiValue()).thenReturn(16.0);
+        Alarm alarm = new Alarm(sensor);
+
+        alarm.check();
+
+        assertTrue(alarm.isAlarmOn());
+    }
+
+    @Test
+    public void alarmIsOffWhenSensorReturnsValueWithinThreshold() {
+        Sensor sensor = mock(Sensor.class);
+        when(sensor.popNextPressurePsiValue()).thenReturn(18.0);
+        Alarm alarm = new Alarm(sensor);
+
+        alarm.check();
+
         assertFalse(alarm.isAlarmOn());
+    }
+
+    @Test
+    public void alarmIsOnWhenSensorReturnsValueIsAboveThreshold() {
+        Sensor sensor = mock(Sensor.class);
+        when(sensor.popNextPressurePsiValue()).thenReturn(30.0);
+        Alarm alarm = new Alarm(sensor);
+
+        alarm.check();
+
+        assertTrue(alarm.isAlarmOn());
     }
 }
