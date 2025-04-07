af630c44fe79ebeb5ac7c1cc6b18113a71803191
Kristian Jakobsen <kristian@nymann.dev>
2025-04-07 22:54:25 +0200

    Format

--- Java/TirePressureMonitoringSystem/src/main/java/tddmicroexercises/tirepressuremonitoringsystem/Alarm.java
+++ Java/TirePressureMonitoringSystem/src/main/java/tddmicroexercises/tirepressuremonitoringsystem/Alarm.java
@@ -1,11 +1,11 @@
 package tddmicroexercises.tirepressuremonitoringsystem;
 
-public class Alarm
-{
+public class Alarm {
     private static final double LOW_PRESSURE_THRESHOLD = 17;
     private static final double HIGH_PRESSURE_THRESHOLD = 21;
 
     protected Sensor sensor;
+
     public Alarm(Sensor sensor) {
         this.sensor = sensor;
     }
@@ -13,18 +13,15 @@ public class Alarm
 
     protected boolean alarmOn = false;
 
-    public void check()
-    {
+    public void check() {
         double psiPressureValue = sensor.popNextPressurePsiValue();
 
-        if (psiPressureValue < LOW_PRESSURE_THRESHOLD || HIGH_PRESSURE_THRESHOLD < psiPressureValue)
-        {
+        if (psiPressureValue < LOW_PRESSURE_THRESHOLD || HIGH_PRESSURE_THRESHOLD < psiPressureValue) {
             alarmOn = true;
         }
     }
 
-    public boolean isAlarmOn()
-    {
-        return alarmOn; 
+    public boolean isAlarmOn() {
+        return alarmOn;
     }
 }
